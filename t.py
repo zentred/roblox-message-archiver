a1f33af82a60bb615aaf49e278dfe17b230e152e
zentred <87871486+zentred@users.noreply.github.com>
2022-04-14 10:24:06 +0100

    Update t.py

--- t.py
+++ t.py
@@ -2,15 +2,17 @@ import requests, random, ctypes
 from threading import Thread
 
 cookie = ''
+
 req = requests.Session()
 req.cookies['.ROBLOSECURITY'] = cookie
 
 totalPages = req.get('https://privatemessages.roblox.com/v1/messages?messageTab=inbox&pageNumber=0&pageSize=20').json()['totalPages']
 messagesFailed = messagesArchived = messagesSkipped = 0
+botMessages = ['our bot noticed', 'cashout', 'model']
 
 def cmdTitle():
     while True:
-        ctypes.windll.kernel32.SetConsoleTitleW(f'Messages Archived: {messagesArchived} | Messages Skipped: {messagesSkipped} | Messages Failed: {messagesFailed}')
+        ctypes.windll.kernel32.SetConsoleTitleW(f'Messages Archived: {messagesArchived} | Messages Skipped: {messagesSkipped} | Messages Failed: {messagesFailed} | if it spams ONLY skipped messages, its finished')
 
 def findMessages():
     global messagesSkipped
@@ -24,7 +26,15 @@ def findMessages():
             for message in data:
                 if message['sender']['id'] == 1:
                     toArchive.append(message['id'])
-                else: messagesSkipped += 1
+                else:
+                    skip = False
+                    for botMessage in botMessages:
+                        if botMessage in message['body'].lower():
+                            toArchive.append(message['id'])
+                            skip = True
+                            continue
+                    if skip == False:
+                        messagesSkipped += 1
             if len(toArchive) != 0:
                 archiveMessages(toArchive)
 
