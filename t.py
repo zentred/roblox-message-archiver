31408471509128ed53a10d137b4554f0b18cf793
zentred <87871486+zentred@users.noreply.github.com>
2022-04-14 10:44:53 +0100

    Update t.py

--- t.py
+++ t.py
@@ -18,36 +18,35 @@ def findMessages():
     global messagesSkipped
     nextPage = random.randint(0, int(totalPages))
     while True:
-        firstPage = req.get(f'https://privatemessages.roblox.com/v1/messages?messageTab=inbox&pageNumber={nextPage}&pageSize=20')
-        if firstPage.status_code == 200:
-            data = firstPage.json()['collection']
-            nextPage = random.randint(0, firstPage.json()['totalPages'])
-            toArchive = []
-            for message in data:
-                if message['sender']['id'] == 1:
-                    toArchive.append(message['id'])
-                else:
-                    skip = False
-                    for botMessage in botMessages:
-                        if botMessage in message['body'].lower():
-                            toArchive.append(message['id'])
-                            skip = True
-                            continue
-                    if skip == False:
-                        messagesSkipped += 1
-            if len(toArchive) != 0:
-                archiveMessages(toArchive)
+        try:
+            firstPage = req.get(f'https://privatemessages.roblox.com/v1/messages?messageTab=inbox&pageNumber={nextPage}&pageSize=20')
+            if firstPage.status_code == 200:
+                data = firstPage.json()['collection']
+                nextPage = random.randint(0, firstPage.json()['totalPages'])
+                toArchive = []
+                for message in data:
+                    if message['sender']['id'] == 1:
+                        toArchive.append(message['id'])
+                    else:
+                        skip = False
+                        [(toArchive.append(message['id']), skip := True) for botMessage in botMessages if botMessage in message['body'].lower()]
+                        if skip == False: messagesSkipped += 1
+                if len(toArchive) != 0:
+                    archiveMessages(toArchive)
+        except: pass
 
 def grabCsrf():
     return req.post('https://auth.roblox.com/v1/logout').headers['X-CSRF-TOKEN']
 
 def archiveMessages(toArchive):
     global messagesArchived, messagesFailed
-    currentCsrf = grabCsrf()
-    json = {"messageIds":toArchive}
-    archiveReq = req.post('https://privatemessages.roblox.com/v1/messages/archive', json=json, headers={'X-CSRF-TOKEN': currentCsrf})
-    if archiveReq.json()['failedMessages'] == []: messagesArchived += len(toArchive)
-    else: messagesFailed += len(archiveReq.json()['failedMessages'])
+    try:
+        currentCsrf = grabCsrf()
+        json = {"messageIds":toArchive}
+        archiveReq = req.post('https://privatemessages.roblox.com/v1/messages/archive', json=json, headers={'X-CSRF-TOKEN': currentCsrf})
+        if archiveReq.json()['failedMessages'] == []: messagesArchived += len(toArchive)
+        else: messagesFailed += len(archiveReq.json()['failedMessages'])
+    except: pass
 
 Thread(target=cmdTitle).start()
 for i in range(10):
